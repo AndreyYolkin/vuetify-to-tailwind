3e3ff4061c34486037aa65270b1fc8c06f93cc52
Andrei Elkin <andreyyolkin@gmail.com>
2025-05-16 23:46:05 +0300

    fix: update margin and padding generators to be tailwind-compatible

--- src/constants/margin.ts
+++ src/constants/margin.ts
@@ -4,19 +4,23 @@ import { spacers } from './_spacers'
 
 const values = spacers.concat(['auto'])
 
-const prefixes = ['ma', 'mx', 'my', 'mt', 'mb', 'ml', 'ms', 'me'] as const
+const prefixes = ['mx', 'my', 'mt', 'mb', 'ml', 'ms', 'me'] as const
 
 export const marginMap = values.reduce((acc, value) => {
+  acc[`ma-${value}`] = `m-${value}`
   if (value !== 0) {
     for (const prefix of prefixes) {
       acc[`${prefix}-n${value}`] = `${prefix}--${value}`
     }
+    acc[`ma-n${value}`] = `m--${value}`
   }
   for (const [i, bp] of vuetifyBreakpoints.entries()) {
     for (const prefix of prefixes) {
       acc[`${prefix}-${bp}-${value}`] = `${tailwindBreakpoints[i]}:${prefix}-${value}`
       acc[`${prefix}-${bp}-n${value}`] = `${tailwindBreakpoints[i]}:${prefix}--${value}`
     }
+    acc[`ma-${bp}-${value}`] = `${tailwindBreakpoints[i]}:m-${value}`
+    acc[`ma-${bp}-n${value}`] = `${tailwindBreakpoints[i]}:m--${value}`
   }
   return acc
 }, {} as Record<string, string>)
--- src/constants/padding.ts
+++ src/constants/padding.ts
@@ -2,13 +2,15 @@ import { tailwindBreakpoints, vuetifyBreakpoints } from './_breakpoints'
 
 import { spacers as values } from './_spacers'
 
-const prefixes = ['pa', 'px', 'py', 'pt', 'pb', 'pl', 'ps', 'pe'] as const
+const prefixes = ['px', 'py', 'pt', 'pb', 'pl', 'ps', 'pe'] as const
 
 export const paddingMap = values.reduce((acc, value) => {
+  acc[`pa-${value}`] = `p-${value}`
   for (const [i, bp] of vuetifyBreakpoints.entries()) {
     for (const prefix of prefixes) {
       acc[`${prefix}-${bp}-${value}`] = `${tailwindBreakpoints[i]}:${prefix}-${value}`
     }
+    acc[`pa-${bp}-${value}`] = `${tailwindBreakpoints[i]}:p-${value}`
   }
   return acc
 }, {} as Record<string, string>)
