63c2707ef8c318571bc7bf551a6359c382a0abf8
Andrei Elkin <andreyyolkin@gmail.com>
2025-05-16 23:18:17 +0300

    chore: unify variables naming

--- src/constants/float.ts
+++ src/constants/float.ts
@@ -1,10 +1,10 @@
 import { tailwindBreakpoints, vuetifyBreakpoints } from './_breakpoints'
 
-const floatOptions = 'left right none start end'.split(' ')
+const values = 'left right none start end'.split(' ')
 
-export const floatMap = floatOptions.reduce((acc, option) => {
+export const floatMap = values.reduce((acc, value) => {
   for (const [i, bp] of vuetifyBreakpoints.entries()) {
-    acc[`float-${bp}-${option}`] = `${tailwindBreakpoints[i]}:float-${option}`
+    acc[`float-${bp}-${value}`] = `${tailwindBreakpoints[i]}:float-${value}`
   }
   return acc
 }, {} as Record<string, string>)
