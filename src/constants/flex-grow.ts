d6291e22458706c0cddc98e4193e0e1c2f4ddfcf
Andrei Elkin <andreyyolkin@gmail.com>
2025-05-16 20:56:38 +0300

    fix: correctly define vuetify flex grow/shrink classes

--- src/constants/flex-grow.ts
+++ src/constants/flex-grow.ts
@@ -1,14 +1,11 @@
 import { tailwindBreakpoints, vuetifyBreakpoints } from './_breakpoints'
 
-const suffixes = {
-  0: '0',
-  1: '1',
-}
+const values = [0, 1]
 
-export const flexGrowMap = Object.entries(suffixes).reduce((acc, [key, value]) => {
-  acc[`flex-grow-${key}`] = `flex-grow-${value}`
+export const flexGrowMap = values.reduce((acc, value) => {
+  acc[`flex-grow-${value}`] = `flex-grow-${value}`
   for (const [i, bp] of vuetifyBreakpoints.entries()) {
-    acc[`flex-grow-${bp}-${key}`] = `${tailwindBreakpoints[i]}:flex-grow-${value}`
+    acc[`flex-${bp}-grow-${value}`] = `${tailwindBreakpoints[i]}:flex-grow-${value}`
   }
   return acc
 }, {} as Record<string, string>)
--- src/constants/flex-shrink.ts
+++ src/constants/flex-shrink.ts
@@ -1,14 +1,11 @@
 import { tailwindBreakpoints, vuetifyBreakpoints } from './_breakpoints'
 
-const suffixes = {
-  0: '0',
-  1: '1',
-}
+const values = [0, 1]
 
-export const flexShrinkMap = Object.entries(suffixes).reduce((acc, [key, value]) => {
-  acc[`flex-shrink-${key}`] = `flex-shrink-${value}`
+export const flexShrinkMap = values.reduce((acc, value) => {
+  acc[`flex-shrink-${value}`] = `flex-shrink-${value}`
   for (const [i, bp] of vuetifyBreakpoints.entries()) {
-    acc[`flex-shrink-${bp}-${key}`] = `${tailwindBreakpoints[i]}:flex-shrink-${value}`
+    acc[`flex-${bp}-shrink-${value}`] = `${tailwindBreakpoints[i]}:flex-shrink-${value}`
   }
   return acc
 }, {} as Record<string, string>)
