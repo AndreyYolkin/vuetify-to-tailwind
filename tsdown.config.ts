8e26caa33ce41e5f90953bc768a9a6cfc822ac14
Andrei Elkin <andreyyolkin@gmail.com>
2025-05-16 20:49:41 +0300

    chore: lint

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -6,17 +6,17 @@ export default defineConfig({
     plugins: [
       {
         name: 'vue-metamorph',
-        transform(code, id) {
+        transform (code, id) {
           if (id.endsWith('vue-metamorph/dist/main.js')) {
             code = code.replaceAll('originalNode.range[', 'originalNode.range?.[')
             code = code.replace(`});
-      if (rootNodeChanged) {`, 
-      `}).filter(({node}) => node.type);
+      if (rootNodeChanged) {`,
+            `}).filter(({node}) => node.type);
       if (rootNodeChanged) {`)
             return code
           }
-        }
-      }
-    ]
-  }
+        },
+      },
+    ],
+  },
 })
